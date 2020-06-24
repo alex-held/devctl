77fe7df39bc99e40c70c79292a3c24bad9a8ab1a
alex held <git@alexanderheld.net>
2020-06-24 22:15:16 +0200

    test(config): remove TestReadPrefixedEnvironmentVariables

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -2,12 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"path"
 	"testing"
 
 	"github.com/gosuri/uitable"
-	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 
 	"github.com/alex-held/dev-env/meta"
@@ -28,34 +25,6 @@ func TestReadConfig(t *testing.T) {
 
 	environment := config.Sub("environment").Sub("vars")
 	PrintTable("environment.vars", environment.AllSettings())
-}
-
-type globalConfig struct {
-	ErrorFile  string
-	DevEnvHome string
-}
-
-var GlobalConfig = globalConfig{}
-
-func TestReadPrefixedEnvironmentVariables(t *testing.T) {
-	config := viper.New()
-	config.SetConfigType("json")
-	config.SetConfigFile("devenv")
-	config.SetEnvPrefix("DEVENV")
-	home, err := homedir.Dir()
-	require.NoError(t, err)
-	config.AddConfigPath(path.Join(home, ".devenv"))
-
-	if err = config.ReadInConfig(); err != nil {
-		_ = fmt.Errorf("Failed to read the configuration file: %s ", err)
-		require.NoError(t, err)
-	}
-
-	if err = config.Unmarshal(&GlobalConfig); err != nil {
-		_ = fmt.Errorf("Failed to unmarshal the configuration file: %s ", err)
-		require.NoError(t, err)
-	}
-
 }
 
 func PrintTable(title string, settings map[string]interface{}) {
