1b27480cb629180967a56c1846812e50d1d502bd
alex-held <git@alexheld.io>
2021-02-13 22:01:53 +0100

    test(config-load): update to bdd tests

--- pkg/testutils/teardown_test.go
+++ pkg/testutils/teardown_test.go
@@ -2,7 +2,7 @@ package testutils
 
 import (
 	"testing"
-	
+
 	"github.com/franela/goblin"
 	. "github.com/onsi/gomega"
 )
@@ -11,7 +11,6 @@ func TestTeardownCombine_Combination_Contains_All_Teardowns(t *testing.T) {
 	g := goblin.Goblin(t)
 	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
 	g.Describe("Teardown", func() {
-		
 		g.It("WHEN CombineInto is called with one additional teardown => THEN returns new combinator function", func() {
 			firstCalled := false
 			secondCalled := false
@@ -21,14 +20,14 @@ func TestTeardownCombine_Combination_Contains_All_Teardowns(t *testing.T) {
 			var second Teardown = func() {
 				secondCalled = true
 			}
-			
+
 			combination := first.CombineInto(second)
 			combination()
-			
+
 			Expect(firstCalled).To(BeTrue())
 			Expect(secondCalled).To(BeTrue())
 		})
-		
+
 		g.It("WHEN CombineInto is called with multiple additional teardown => THEN returns new combinator function", func() {
 			firstCalled := false
 			secondCalled := false
@@ -42,10 +41,10 @@ func TestTeardownCombine_Combination_Contains_All_Teardowns(t *testing.T) {
 			var third Teardown = func() {
 				thirdCalled = true
 			}
-			
+
 			combination := first.CombineInto(second, third)
 			combination()
-			
+
 			Expect(firstCalled).To(BeTrue())
 			Expect(secondCalled).To(BeTrue())
 			Expect(thirdCalled).To(BeTrue())
