052e0db13cf20d882a3840b55a6fb9c1a1540903
alex-held <git@alexheld.io>
2021-02-14 20:09:55 +0100

    feat(cmd-sdkman): download sdk's with version or default endpoint

--- internal/sdkman/download.go
+++ internal/sdkman/download.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-
+	"path/filepath"
+	
+	"github.com/alex-held/devctl/pkg/cli"
 	"github.com/coreos/etcd/pkg/fileutil"
 	"github.com/spf13/afero"
-
+	
 	"github.com/alex-held/devctl/pkg/aarch"
 )
 
@@ -21,6 +23,20 @@ type SDKDownload struct {
 // DownloadService downloads SDKs to the filesystem
 type DownloadService service
 
+func (s *DownloadService) Resolve() func(...string) string {
+	c := cli.GetOrCreateCLI()
+	rootDir := c.ConfigDir()
+	
+	return func(paths ...string) (fp string) {
+		fp = filepath.Join(rootDir, string(cli.Downloads))
+		
+		for _, p := range paths {
+			fp = filepath.Join(fp, p)
+		}
+		return fp
+	}
+}
+
 // DownloadSDK downloads the sdk from the sdkman broker.
 // SDK specifies the sdk
 // Version specifies the apiVersion
@@ -28,43 +44,88 @@ type DownloadService service
 // https://api.sdkman.io/2/broker/download/scala/scala-2.13.4/darwinx64
 // https://api.sdkman.io/2/broker/download/scala/2.13.4/darwinx64
 // nolint: lll
-func (s *DownloadService) DownloadSDK(ctx context.Context, filepath, sdk, version string, arch aarch.Arch) (*SDKDownload, *http.Response, error) {
+func (s *DownloadService) DownloadSDK(ctx context.Context, dlPath, sdk, version string, arch aarch.Arch) (*SDKDownload, *http.Response, error) {
 	switch arch {
 	case aarch.Linux64, aarch.MacOsx, aarch.LinuxArm32:
-		// CreateDownloadSDK creates the URI to Download SDK archives from SDKMAN API
-		// https://api.sdkman.io/2/broker/download/scala/2.13.4/darwinx64
-		// "broker", "Download", sdk, version, arch
-		req, err := s.client.NewRequest(ctx, "GET", fmt.Sprintf("broker/download/%s/%s/%s", sdk, version, arch), nil)
-
+		req, err := s.client.NewRequest(ctx, "GET", fmt.Sprintf("broker/download/%s/%s/%s", sdk, version, string(arch)), http.NoBody)
 		if err != nil {
 			return nil, nil, err
 		}
-
 		resp, err := s.client.client.Do(req)
 		if err != nil {
 			return nil, resp, err
 		}
-
-		var buf bytes.Buffer
-		if _, err = buf.ReadFrom(resp.Body); err != nil {
-			return nil, nil, err
-		}
-
-		defer resp.Body.Close()
-
-		body := buf.Bytes()
-
-		err = afero.WriteFile(s.client.fs, filepath, body, fileutil.PrivateDirMode)
+		
+		
+		var body bytes.Buffer
+		io.Copy(&body, resp.Body)
+		dump := body.Bytes()
+		// dump, err := httputil.DumpResponse(resp, false)
+		err = afero.WriteFile(s.client.fs, dlPath+".zip", dump, fileutil.PrivateFileMode)
 		if err != nil {
-			return nil, resp, err
+			panic(err)
+		} else {
+			downloadFile, err := s.client.fs.Open(dlPath + ".zip")
+			if err != nil {
+				panic(err)
+			}
+			download := &SDKDownload{
+				Path:   dlPath + ".zip",
+				Reader: downloadFile,
+			}
+			return download, resp, nil
 		}
-
-		download := &SDKDownload{
-			Path:   filepath,
-			Reader: bytes.NewBuffer(body),
+		
+		// download directory does not exist. -> creating ...
+		if exists, err := afero.DirExists(s.client.fs, filepath.Dir(dlPath)); !exists {
+			if err != nil {
+				return nil, nil, err
+			}
+			
+			err = s.client.fs.MkdirAll(filepath.Dir(dlPath), 0755)
+			if err != nil {
+				return nil, nil, err
+			}
 		}
-		return download, resp, err
+		
+		var downloadFile afero.File
+		if ex, err := afero.Exists(s.client.fs, dlPath+".zip"); !ex {
+			if err != nil {
+				return nil, nil, err
+			}
+			downloadFile, err = afero.NewOsFs().Create(dlPath + ".zip")
+			if err != nil {
+				return nil, nil, err
+			}
+			
+			n, err := io.Copy(downloadFile, resp.Body)
+			if err != nil {
+				return nil, nil, err
+			}
+			fmt.Printf("copied %d bytes", n)
+			
+			download := &SDKDownload{
+				Path:   dlPath,
+				Reader: downloadFile,
+			}
+			return download, resp, nil
+		} else {
+			fmt.Printf("sdk is already downloaded at '%s'", dlPath+".zip")
+			downloadFile, err := s.client.fs.Open(dlPath + ".zip")
+			if err != nil {
+				return nil, nil, err
+			}
+			download := &SDKDownload{
+				Path:   dlPath,
+				Reader: downloadFile,
+			}
+			return download, resp, nil
+		}
 	default:
 		return nil, nil, fmt.Errorf("right now the provided aarc.Arch '%s' is not supported", arch)
 	}
+}
+
+type FilePathResolver interface {
+	Resolve() string
 }
--- internal/sdkman/version.go
+++ internal/sdkman/version.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	
+
 	"github.com/blang/semver"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -18,14 +18,14 @@ var logger = log.New()
 
 func (s *VersionService) Default(ctx context.Context, sdk string) (version *semver.Version, err error) {
 	req, err := s.client.NewRequest(ctx, "GET", fmt.Sprintf("candidates/default/%s", sdk), http.NoBody)
-	
+
 	if err != nil {
 		logger.
 			WithError(err).
 			WithField("sdk", sdk).
 			Errorln("could not build http.Request for VersionService.Default")
 	}
-	
+
 	resp, err := s.client.client.Do(req)
 	if err != nil {
 		logger.
@@ -34,7 +34,7 @@ func (s *VersionService) Default(ctx context.Context, sdk string) (version *semv
 			WithField("url", req.URL.String()).
 			Errorln("http.Request failed for VersionService.Default")
 	}
-	
+
 	var buf bytes.Buffer
 	if _, err = buf.ReadFrom(resp.Body); err != nil {
 		return nil, errors.Wrap(err, "read from response body failed")
@@ -47,9 +47,11 @@ func (s *VersionService) Default(ctx context.Context, sdk string) (version *semv
 		logger.
 			WithError(err).
 			WithField("body", body).
+			WithField("url", req.URL.String()).
+			WithField("sdk", sdk).
 			Errorln("response is no valid semver")
 		return nil, err
 	}
-	
+
 	return &sdkSemVer, err
 }
