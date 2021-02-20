8eee86b79efb9ee5eb54d4b81ca79ee9e1d4cfde
dev <alex-held>
2021-02-20 02:47:52 +0100

    feat(cmd-sdkman): download sdk's with version or default endpoint

--- cmd/sdkman.go
+++ cmd/sdkman.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/alex-held/devctl/pkg/aarch"
+	"github.com/blang/semver"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	"github.com/alex-held/devctl/internal/sdkman"
@@ -29,7 +34,11 @@ func NewSdkManCommand() (cmd *cobra.Command) {
 		},
 	}
 
-	cmd.AddCommand(NewSdkManListCommand())
+	cmd.AddCommand(
+		NewSdkManListCommand(),
+		NewSdkManDefaultCommand(),
+		NewSdkManDownloadCommand(),
+	)
 	return cmd
 }
 
@@ -67,5 +76,80 @@ func NewSdkManListCommand() (cmd *cobra.Command) {
 	}
 
 	cmd.Flags().StringVarP(&fmtFlag, "format", "f", string(Table), "the output format of the cli app. -format=table")
+	return cmd
+}
+
+// NewSdkManCommand creates the sdkman command
+func NewSdkManDefaultCommand() (cmd *cobra.Command) {
+	cmd = &cobra.Command{
+		Use:       "default",
+		Short:     "Displays the latest (default) version of a given  sdk ",
+		ValidArgs: strings.Split("ant,asciidoctorj,ballerina,bpipe,btrace,ceylon,concurnas,crash,cuba,cxf,doctoolchain,dotty,gaiden,glide,gradle,gradleprofiler,grails,groovy,groovyserv,http4k,infrastructor,java,jbake,jbang,karaf,kotlin,kscript,layrry,lazybones,leiningen,maven,micronaut,mulefd,mvnd,sbt,scala,spark,springboot,sshoogr,test,tomcat,vertx,visualvm", ","),
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx := cmd.Context()
+			client := sdkman.NewSdkManClient()
+			defaultVersion, err := client.Version.Default(ctx, args[0])
+			if err != nil {
+				cli.ExitWithError(1, err)
+			}
+
+			fmt.Println(defaultVersion)
+		},
+	}
+
+	// cmd.Flags().StringVarP(&fmtFlag, "format", "f", string(Table), "the output format of the cli app. -format=table")
+	return cmd
+}
+
+// NewSdkManCommand creates the sdkman command
+func NewSdkManDownloadCommand() (cmd *cobra.Command) {
+	cmd = &cobra.Command{
+		// 	Example: add [-F file | -D dir]... [-f format] profile
+		Use:     `download`,
+		Aliases: []string{"d", "dl"},
+		Short:   "Downloads the sdk  (default) version of a given  sdk ",
+		Long: `
+		download [sdk] #downloads latest (default) version
+		download [sdk] [version]
+		download [sdk] [version] [arch]`,
+		//		ValidArgs: strings.Split("ant,asciidoctorj,ballerina,bpipe,btrace,ceylon,concurnas,crash,cuba,cxf,doctoolchain,dotty,gaiden,glide,gradle,gradleprofiler,grails,groovy,groovyserv,http4k,infrastructor,java,jbake,jbang,karaf,kotlin,kscript,layrry,lazybones,leiningen,maven,micronaut,mulefd,mvnd,sbt,scala,spark,springboot,sshoogr,test,tomcat,vertx,visualvm", ","),
+		Args: cobra.RangeArgs(1, 3),
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx := cmd.Context()
+			client := sdkman.NewSdkManClient()
+
+			switch len(args) {
+			case 0:
+				cli.ExitWithError(1, os.ErrInvalid)
+			case 1:
+				sdk := args[0]
+				version, err := client.Version.Default(ctx, sdk)
+				cli.ExitWithError(1, err)
+				dlPath := client.Download.Resolve()(sdk, version.String())
+
+				dl, r, err := client.Download.DownloadSDK(ctx, dlPath, sdk, version.String(), aarch.MacOsx)
+				cli.ExitWithError(1, err)
+				defer r.Body.Close()
+				fmt.Printf("Downloaded sdk to path: %s", dl.Path)
+				os.Exit(0)
+			case 2:
+				sdk := args[0]
+				version, err := semver.ParseTolerant(args[1])
+				cli.ExitWithError(1, err)
+				dlPath := client.Download.Resolve()(sdk, version.String())
+
+				dl, r, err := client.Download.DownloadSDK(ctx, dlPath, sdk, version.String(), aarch.MacOsx)
+				cli.ExitWithError(1, err)
+				defer r.Body.Close()
+				fmt.Printf("Downloaded sdk to path: %s", dl.Path)
+				os.Exit(0)
+			default:
+				cli.ExitWithError(1, errors.New("sdkman download called with too many arguments"))
+
+			}
+		},
+	}
+
+	// cmd.Flags().StringVarP(&fmtFlag, "format", "f", string(Table), "the output format of the cli app. -format=table")
 	return cmd
 }
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/alex-held/devctl/cmd"
+import (
+	"github.com/alex-held/devctl/cmd"
+)
 
 func main() {
+	/*
+		header := &doc.GenManHeader{
+			Title:   "MINE",
+			Section: "3",
+		}
+		err := doc.GenManTree(cmd.rootCmd, header, "/Users/dev/.devctl/tmp")
+		if err != nil {
+			log.Default().Fatal(err)
+		}*/
 	cmd.Execute()
 }
