d19972dc2e586d54cf2221786a01ae0f2422a6b7
alex-held <git@alexheld.io>
2021-04-03 03:53:27 +0200

    feat(plugin): try out sdk-plugin

---
+++ pkg/plugins/plugin.go
@@ -0,0 +1,40 @@
+package plugins
+
+import (
+	"errors"
+	"plugin"
+
+	"github.com/alex-held/devctl/pkg/plugins/sdk"
+)
+
+type pluginRegistry struct {
+	SDKPlugins []sdk.SDKPlugin
+}
+
+// Plugin is a plugin loaded from a file
+type DevCtlPlugin struct {
+	// Name of the plugin e.g rabbitmq
+	Name string
+	// Type of the plugin e.g broker
+	Type string
+	// Path specifies the import path
+	Path string
+	// NewFunc creates an instance of the plugin
+	NewFunc interface{}
+}
+
+func (pr *pluginRegistry) Load(p string) (*plugin.Plugin, error) {
+	plug, err := plugin.Open(p)
+	if err != nil {
+		return nil, err
+	}
+	s, err := plug.Lookup("Plugin")
+	if err != nil {
+		return nil, err
+	}
+	pl, ok := s.(*plugin.Plugin)
+	if !ok {
+		return nil, errors.New("could not cast Plugin object")
+	}
+	return pl, nil
+}
