a86a30b4341c60f71f80e8f1dcdb3e5ac1b94319
alex-held <git@alexheld.io>
2021-04-07 13:02:07 +0200

    feat(devctl-sdk-plugin-go): install go sdk using plugin

--- pkg/plugins/sdk/go.go
+++ pkg/plugins/sdk/go.go
@@ -1,26 +1,46 @@
-//+build !windows,!darwin
-
 package sdk
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/blang/semver"
+	"github.com/coreos/etcd/pkg/fileutil"
+	"github.com/pkg/errors"
 	"github.com/spf13/afero"
 
+	"github.com/alex-held/devctl/pkg/constants"
 	"github.com/alex-held/devctl/pkg/devctlpath"
 	"github.com/alex-held/devctl/pkg/plugins"
 )
 
 type devctlSdkpluginGo struct {
-	FS     afero.Fs
-	Pather devctlpath.Pather
+	FS                afero.Fs
+	Pather            devctlpath.Pather
+	HttpClient        http.Client
+	BaseUri           string
+	RuntimeInfoGetter plugins.RuntimeInfoGetter
 }
 
-func (p *devctlSdkpluginGo) NewFunc() plugins.SDKPlugin { return &devctlSdkpluginGo{} }
+func (p *devctlSdkpluginGo) NewFunc() plugins.SDKPlugin {
+	return &devctlSdkpluginGo{
+		FS:                afero.NewOsFs(),
+		Pather:            devctlpath.DefaultPather(),
+		HttpClient:        http.Client{},
+		BaseUri:           "https://golang.org",
+		RuntimeInfoGetter: plugins.OSRuntimeInfoGetter{},
+	}
+}
 
-func (p *devctlSdkpluginGo) Name() string {
+func (*devctlSdkpluginGo) Name() string {
 	return "devctl-sdkplugin-go"
 }
 
@@ -33,7 +53,7 @@ func (p *devctlSdkpluginGo) ListVersions() (versions []string) {
 	}
 	for _, fileInfo := range fileInfos {
 		dirname := fileInfo.Name()
-		if dirname == "current" {
+		if dirname == "current" || fileInfo.Mode().Type() == os.ModeSymlink {
 			continue
 		}
 
@@ -42,7 +62,7 @@ func (p *devctlSdkpluginGo) ListVersions() (versions []string) {
 		}
 	}
 
-	fmt.Printf("found versions: %v", versions)
+	fmt.Printf("found versions: %v\n", versions)
 	return versions
 }
 
@@ -54,10 +74,144 @@ func (p *devctlSdkpluginGo) isValidVersion(dirname string) (version string, vali
 	return dirname, true
 }
 
-func (p *devctlSdkpluginGo) Download(version string) {
-	fmt.Printf("downloading go sdk version %s;", version)
+func (p *devctlSdkpluginGo) Download(version string) (err error) {
+	dlPath := p.Pather.Download("go", version)
+	err = p.FS.MkdirAll(dlPath, fileutil.PrivateDirMode)
+	if err != nil {
+		return errors.Wrapf(err, "failed creating go sdk download path; version=%s; err=%v", version, err)
+	}
+	filename := fmt.Sprintf("go%s.darwin-amd64.tar.gz", version)
+	dlURI := fmt.Sprintf("%s/dl/%s", p.BaseUri, filename)
+	response, err := p.HttpClient.Get(dlURI)
+	if err != nil {
+		return errors.Wrapf(err, "failed downloading go sdk archive; version=%s; err=%v", version, err)
+	}
+	defer response.Body.Close()
+	filePath := path.Join(dlPath, filename)
+	err = afero.WriteReader(p.FS, filePath, response.Body)
+	if err != nil {
+		return errors.Wrapf(err, "failed writing go sdk archive; version=%s; err=%v", version, err)
+	}
+	return nil
+}
+
+func (p *devctlSdkpluginGo) Link(version string) (err error) {
+	sdkPath := p.Pather.SDK("go", version)
+	current := p.Pather.SDK("go", "current")
+	if ok, _ := afero.DirExists(p.FS, current); ok {
+		_ = p.FS.Remove(current)
+	}
+
+	symlink := exec.Command("ln", "-s", sdkPath, current)
+	err = symlink.Run()
+	if err != nil {
+		return errors.Wrapf(err, "failed linking go sdk %s; src=%s; dest=%s\n", version, sdkPath, current)
+	}
+	return nil
+}
+
+func (p *devctlSdkpluginGo) archiveName(version string) string {
+	ri := p.RuntimeInfoGetter.Get()
+	return fmt.Sprintf("go%s.%s-%s.tar.gz", version, ri.OS, ri.Arch)
+}
+
+func (p *devctlSdkpluginGo) Extract(version string) (err error) {
+	archiveName := p.archiveName(version)
+	archivePath := p.Pather.Download("go", version, archiveName)
+	sdkPath := p.Pather.SDK("go", version)
+	archive, err := p.FS.Open(archivePath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open go sdk archive=%s\n", archivePath)
+	}
+	err = p.FS.MkdirAll(p.Pather.SDK("go", version), fileutil.PrivateDirMode)
+	if err != nil {
+		return errors.Wrapf(err, "failed to Extract go sdk %s; dest=%s; archive=%s\n", version, sdkPath, archivePath)
+	}
+	err = UnTarGzip(archive, sdkPath, GoSDKUnarchiveRenamer())
+	if err != nil {
+		return errors.Wrapf(err, "failed to Extract go sdk %s; dest=%s; archive=%s\n", version, sdkPath, archivePath)
+	}
+	return err
+}
+
+func GoSDKUnarchiveRenamer() Renamer {
+	return func(p string) string {
+		parts := strings.Split(p, string(filepath.Separator))
+		parts = parts[1:]
+		newPath := strings.Join(parts, string(filepath.Separator))
+		return newPath
+	}
+}
+
+func (p *devctlSdkpluginGo) InstallE(version string) (err error) {
+	if exists, _ := afero.DirExists(p.FS, p.Pather.SDK("go", version)); exists {
+		err = p.Link(version)
+		if err != nil {
+			return errors.Wrapf(err, "failed to install go sdk %s", version)
+		}
+	}
+	if exists, _ := afero.Exists(p.FS, p.Pather.Download("go", version, p.archiveName(version))); !exists {
+		err = p.Download(version)
+		if err != nil {
+			return errors.Wrapf(err, "failed to install go sdk %s", version)
+		}
+	}
+	err = p.Extract(version)
+	if err != nil {
+		return errors.Wrapf(err, "failed to install go sdk %s", version)
+	}
+	err = p.Link(version)
+	if err != nil {
+		return errors.Wrapf(err, "failed to install go sdk %s", version)
+	}
+	return nil
 }
 
 func (p *devctlSdkpluginGo) Install(version string) {
-	fmt.Printf("installing go sdk version %s;", version)
+	err := p.InstallE(version)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(constants.Failure)
+	}
+}
+
+type Renamer func(p string) string
+
+func UnTarGzip(file afero.File, target string, renamer Renamer) error {
+	gr, _ := gzip.NewReader(file)
+	tr := tar.NewReader(gr)
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		filename := header.Name
+		if renamer != nil {
+			filename = renamer(filename)
+		}
+
+		p := filepath.Join(target, filename)
+		fi := header.FileInfo()
+
+		if fi.IsDir() {
+			if err = os.MkdirAll(p, fi.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+		file, err := os.OpenFile(p, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, fi.Mode())
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(file, tr)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- pkg/plugins/types.go
+++ pkg/plugins/types.go
@@ -1,9 +1,36 @@
 package plugins
 
+import (
+	"runtime"
+)
+
 type SDKPlugin interface {
 	Name() string
 	ListVersions() []string
-	Download(version string)
+	InstallE(version string) (err error)
+	Download(version string) (err error)
 	Install(version string)
 	NewFunc() SDKPlugin
+}
+
+type RuntimeInfo struct {
+	OS, Arch string
+}
+
+type RuntimeInfoGetter interface {
+	Get() (info RuntimeInfo)
+}
+
+type OSRuntimeInfoGetter struct{}
+
+func (OSRuntimeInfoGetter) Get() (info RuntimeInfo) {
+	osId := runtime.GOOS
+	archId := runtime.GOARCH
+	if archId == "arm" {
+		archId = "arm64"
+	}
+	return RuntimeInfo{
+		OS:   osId,
+		Arch: archId,
+	}
 }
