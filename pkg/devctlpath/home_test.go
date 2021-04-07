8b1f44538caf055cfb9c34a92aef77eec2d21e48
alex-held <git@alexheld.io>
2021-04-07 14:24:41 +0200

    fix(devctl-plugin-sdk-go): only set XDG_CACHE_HOME on unix machine

--- pkg/devctlpath/home_test.go
+++ pkg/devctlpath/home_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"runtime"
 	"strings"
 	"testing"
 	"testing/quick"
@@ -80,7 +81,10 @@ func TestHomeFinder(t *testing.T) {
 	/* CacheDir */
 	g.Describe("Cache", func() {
 		g.It("WHEN no pathFn set", func() {
-			_ = os.Setenv("XDG_CACHE_HOME", "/tmp/cache")
+			if runtime.GOOS == "linux" {
+				_ = os.Setenv("XDG_CACHE_HOME", "/tmp/cache")
+			}
+
 			cacheDir, _ := os.UserCacheDir()
 			expected := filepath.Join(cacheDir, "io.alexheld.test_devctl")
 			lazyFinder = NewPather(WithAppPrefix(testAppPrefix))
