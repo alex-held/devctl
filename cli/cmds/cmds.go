de02527624903b1f3d976c974f22d9ce84fb3ee1
alex-held <git@alexheld.io>
2021-04-23 13:17:14 +0200

    refactor: move meta function into cli

--- cli/cmds/cmds.go
+++ cli/cmds/cmds.go
@@ -1,9 +1,12 @@
 package cmds
 
 import (
-	"github.com/gobuffalo/plugins"
+	"bytes"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
 
-	"github.com/alex-held/devctl/meta"
+	"github.com/gobuffalo/plugins"
 
 	"github.com/alex-held/devctl/cli/cmds/sdk"
 	"github.com/alex-held/devctl/cli/cmds/version"
@@ -24,7 +27,7 @@ func AvailablePlugins(root string) []plugins.Plugin {
 	plugs = append(plugs, sdk.Plugins()...)
 	plugs = append(plugs, golang.Plugins()...)
 
-	if meta.IsDevctl(root) {
+	if IsDevctl(root) {
 		plugs = append(plugs, insidePlugins()...)
 		return plugs
 	}
@@ -43,4 +46,18 @@ func insidePlugins() []plugins.Plugin {
 	var plugs []plugins.Plugin
 	plugs = append(plugs, golang.Plugins()...)
 	return plugs
+}
+
+
+func IsDevctl(mod string) bool {
+	if !strings.HasPrefix(mod, "go.mod") {
+		mod = filepath.Join(mod, "go.mod")
+	}
+
+	b, err := ioutil.ReadFile(mod)
+	if err != nil {
+		return false
+	}
+
+	return bytes.Contains(b, []byte("github.com/alex-held/devctl"))
 }
