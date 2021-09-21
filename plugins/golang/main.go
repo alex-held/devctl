8cc2fad0b007bd824c9a64ae0bcc65942656aa34
alex-held <git@alexheld.io>
2021-09-22 01:24:24 +0200

    feat(plugins/golang): add install, list and current commands

--- plugins/golang/main.go
+++ plugins/golang/main.go
@@ -1,16 +1,26 @@
 package golang
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"context"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/alex-held/devctl/pkg/devctlpath"
+	"github.com/alex-held/devctl/pkg/system"
+	"github.com/spf13/afero"
 )
 
 type Config struct {
 	InstallPath string `yaml:"install_path"`
+	Fs          afero.Fs
 }
 
 // CreateConfig creates the default plugin config
@@ -27,7 +37,7 @@ func CreateConfig(devctlPath string) map[string]string {
 const goSDKInstallPathKey = "goSDKInstallPath"
 
 func New(cfgMap map[string]string, args []string) (err error) {
-	args = args[1:]
+	args = args[0:]
 
 	cfg := &Config{InstallPath: cfgMap[goSDKInstallPathKey]}
 
@@ -41,22 +51,22 @@ func New(cfgMap map[string]string, args []string) (err error) {
 
 	switch subcmd {
 	case "list":
-		if err = validateArgsForSubcommand(subcmd, args, 1); err != nil {
+		if err = validateArgsForSubcommand(subcmd, args, 0); err != nil {
 			return err
 		}
 		return handleList(cfg)
 	case "current":
-		if err = validateArgsForSubcommand(subcmd, args, 1); err != nil {
+		if err = validateArgsForSubcommand(subcmd, args, 0); err != nil {
 			return err
 		}
 		return handleCurrent(cfg)
 	case "install":
-		if err = validateArgsForSubcommand(subcmd, args, 2); err != nil {
+		if err = validateArgsForSubcommand(subcmd, args, 1); err != nil {
 			return err
 		}
 		return handleInstall(args[0], cfg)
 	case "use":
-		if err = validateArgsForSubcommand(subcmd, args, 2); err != nil {
+		if err = validateArgsForSubcommand(subcmd, args, 1); err != nil {
 			return err
 		}
 		return handleUse(args[0], cfg)
@@ -65,35 +75,82 @@ func New(cfgMap map[string]string, args []string) (err error) {
 	}
 }
 
-func handleInstall(version string, config *Config) (err error) {
-	installPath := path.Join(config.InstallPath, version)
-	fmt.Printf("[sdk/go] installing go sdk version %s into %s", version, installPath)
+func formatGoArchiveArtifactName(ri system.RuntimeInfo, version string) string {
+	return ri.Format("go%s.[os]-[arch].tar.gz", version)
+}
 
+func dlArchive(version string, fs afero.Fs) (archive *bytes.Buffer, err error) {
+	ri := system.OSRuntimeInfoGetter{}
+	artifactName := formatGoArchiveArtifactName(ri.Get(), strings.TrimPrefix(version, "v"))
+
+	// file, err := afero.TempFile(fs, "", "devctl-plugin-golang-*")
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	dlUri := ri.Get().Format("%s/dl/%s", "https://golang.org", artifactName)
+
+	buf := &bytes.Buffer{}
+	err = download(context.Background(), dlUri, buf)
+	if err != nil {
+		return buf, fmt.Errorf("failed downloading go sdk %v from the remote server %s; err=%v", version, "https://golang.org", err)
+	}
+
+	return buf, nil
+}
+
+func download(ctx context.Context, url string, outWriter io.Writer) (err error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(outWriter, resp.Body)
 	return err
+}
+
+func handleInstall(version string, config *Config) (err error) {
+
+	installPath := path.Join(config.InstallPath, version)
+
+	archive, err := dlArchive(version, config.Fs)
+	if err != nil {
+		return err
+	}
+
+	err = config.Fs.MkdirAll(installPath, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to Extract go sdk %s; dest=%s; archive=%s;err=%v\n", version, installPath, "*Bytes.Buffer", err)
+	}
+	err = unTarGzip(archive, installPath, unarchiveRenamer(), config.Fs)
+	if err != nil {
+		return fmt.Errorf("failed to Extract go sdk %s; dest=%s; archive=%s;err=%v\n", version, installPath, "*Bytes.Buffer", err)
+	}
+	return nil
 }
 
 func handleList(config *Config) (err error) {
 	installPath := path.Join(config.InstallPath)
 	ds, err := os.ReadDir(installPath)
 
-	var installedVersions []string
-
 	if err != nil {
 		return err
 	}
 
+	output := strings.Builder{}
+
 	for _, d := range ds {
-		if d.IsDir() && strings.HasPrefix(d.Name(), "v") {
-			installedVersions = append(installedVersions, d.Name())
+		if d.IsDir() {
+			output.WriteString("v" + d.Name() + "\n")
 		}
 	}
 
-	output := strings.Builder{}
-	for _, version := range installedVersions {
-		output.WriteString(version + "\n")
-	}
-
-	fmt.Printf("[sdk/go] installed versions:\n%s\n", output.String())
+	fmt.Print(output.String())
 	return nil
 }
 
@@ -110,7 +167,9 @@ func handleCurrent(config *Config) (err error) {
 	if err != nil {
 		return err
 	}
-	fmt.Println(link)
+	currentDir := path.Base(link)
+	currentVersion := "v" + currentDir
+	fmt.Println(currentVersion)
 	return nil
 }
 
@@ -123,4 +182,56 @@ func validateArgsForSubcommand(subcmd string, args []string, expected int) error
 		return fmt.Errorf("provided wrong number of argument for subcommand '%s'; expected=%d; provided=%d", subcmd, expected, len(args))
 	}
 	return nil
+}
+
+func unTarGzip(buf *bytes.Buffer, target string, renamer Renamer, fs afero.Fs) error {
+	gr, _ := gzip.NewReader(buf)
+	tr := tar.NewReader(gr)
+
+	for {
+		header, err := tr.Next()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		filename := header.Name
+		if renamer != nil {
+			filename = renamer(filename)
+		}
+
+		p := filepath.Join(target, filename)
+		fi := header.FileInfo()
+
+		if fi.IsDir() {
+			if e := fs.MkdirAll(p, fi.Mode()); e != nil {
+				return e
+			}
+			continue
+		}
+		file, err := fs.OpenFile(p, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, fi.Mode())
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(file, tr)
+		file.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type Renamer func(p string) string
+
+func unarchiveRenamer() Renamer {
+	return func(p string) string {
+		parts := strings.Split(p, string(filepath.Separator))
+		parts = parts[1:]
+		newPath := strings.Join(parts, string(filepath.Separator))
+		return newPath
+	}
 }
